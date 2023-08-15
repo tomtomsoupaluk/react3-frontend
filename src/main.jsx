bb34306f1508191ff3999d5f1a3e6d0202bf08de
jackkaphon <jackwchh@gmail.com>
2023-08-15 20:00:28 +0700

    update server path

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import './index.css'
 // window.$api = 'http://localhost:3000'
 
 // production
-// window.$api = 'https://jackapi1.wannabedevs.com'
-window.$api = 'http://5.189.153.72:3003'
+window.$api = 'https://jackapi.wannabedevs.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
